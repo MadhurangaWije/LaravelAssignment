dae86eedc750cdaffe03baac7d83e05bab50f37c
Charith Madusanka <liyanara_se14020@stu.kln.ac.lk>
2018-08-24 12:16:20 +0530

    Update webseed

--- database/seeds/SettingsTableSeeder.php.php
+++ database/seeds/SettingsTableSeeder.php.php
@@ -6,13 +6,12 @@ class SettingsTableSeeder extends Seeder
   public function run()
   {
     App\Setting::create([
-      'site_name'       => 'LaravelCMS',
-      'contact_phone'   => '702-555-1111',
+      'site_name'       => 'LaravelWebAssignment',
+      'contact_phone'   => '0773982666',
       'contact_email'   => 'admin@example.com',
-      'contact_address' => '12345 E. Sunset Blvd, Henderson, NV 89999',
+      'contact_address' => 'University of kelaniya,Dalugama',
       'hours'           => '9am - 5pm',
       'support'         => '702-555-2222',
-      'about'           => 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking.',
-    ]);
+      'about'           => 'Leverage Group WebAssignment'
   }
 }
